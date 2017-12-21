d1b77ad22f81d7d0ae384a33cb75f93631f78477
Andrew Walker <hovercar00@gmail.com>
2017-12-21 04:17:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,9 @@
 for (var i=1; i<25; i++) {
-  if (i === 1);
+  if (i === 1) {
     console.log("I am 1 strange loop.");
-  else;
+  }
+  else {
     console.log("I am ${i} strange loop.");
+  }
 }
+
