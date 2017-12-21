418f5bd5a0380b3920bedf737024b9b027d6506d
Andrew Walker <hovercar00@gmail.com>
2017-12-21 04:29:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,6 +21,9 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
   while (array.length > 0 && maybeTrue());
   return array
 }
